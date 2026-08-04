76075e7984e652048b177198309876f9c4807ce7
miguelguiul1 <miguelananiasalves1@gmail.com>
2026-08-04 17:13:32 -0300

    .

---
+++ HTML e CSS/Aula 25 - React - Prova Surpresa/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import wallpaper from './/wallpaper-frei.jpg';
+function App() {
+  return (
+    <div className="App">
+      <h1>Miguel Ananias</h1>
+      <img src={wallpaper} alt="wallpaper" />
+    </div>
+  );
+}
+
+export default App;
